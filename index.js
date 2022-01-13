94fb33ad3649087bcebe09fede694a3e4f0724d9
Otanga-Otieno <otieno.otanga@gmail.com>
2022-01-13 15:21:57 +0300

    Identify diagonal winning patterns

--- index.js
+++ index.js
@@ -25,9 +25,11 @@ function play(cellId) {
     if((turns%2) == 0) {
         drawO(cellId);
         turns += 1;
+        winPatternSymbol("O");
     } else {
         drawX(cellId);
         turns += 1;
+        winPatternSymbol("X");
     }
 
 }
@@ -40,6 +42,50 @@ function isOccupied(cell) {
         return true;
     } else {
         return false;
+    }
+
+}
+
+function winPattern() {
+
+    var pattern = false;
+
+    var a1 = document.getElementById("a1").innerHTML;
+    var a2 = document.getElementById("a2").innerHTML;
+    var a3 = document.getElementById("a3").innerHTML;
+    var b1 = document.getElementById("b1").innerHTML;
+    var b2 = document.getElementById("b2").innerHTML;
+    var b3 = document.getElementById("b3").innerHTML;
+    var c1 = document.getElementById("c1").innerHTML;
+    var c2 = document.getElementById("c2").innerHTML;
+    var c3 = document.getElementById("c3").innerHTML;
+
+    if ((a1 == b2) && (b2 == c3) && b2 != "") {
+        pattern = true;
+        return pattern;
+    }
+
+    if ((a3 == b2) && (b2 == c1) && b2 != "") {
+        pattern = true;
+        return pattern;
+    }
+
+}
+
+function winPatternSymbol(symbol) {
+
+    if(winPattern()) {
+        var winnerSpan = document.getElementById("winner");
+        var winsSpan = document.getElementById("wins");
+        winnerSpan.innerHTML = symbol;
+        winnerSpan.removeAttribute("hidden");
+        winsSpan.removeAttribute("hidden");
+    }
+
+    if(symbol == "X") {
+        winnerSpan.style.color = "blue";
+    } else {
+        winnerSpan.style.color = "red";
     }
 
 }
