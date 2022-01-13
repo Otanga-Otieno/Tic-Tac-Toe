8ec471a249807b7410d60dea4f9491028e81fb93
Otanga-Otieno <otieno.otanga@gmail.com>
2022-01-13 12:35:42 +0300

    Implement play function
    Determine turn on click and draw the appropriate symbol

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var turns = 1;
+
 function drawX(cell) {
 
     var tablecell = document.getElementById(cell);
@@ -9,5 +11,17 @@ function drawO(cell) {
 
     var tablecell = document.getElementById(cell);
     tablecell.innerHTML = "O";
+
+}
+
+function play(cellId) {
+
+    if((turns%2) == 0) {
+        drawO(cellId);
+        turns += 1;
+    } else {
+        drawX(cellId);
+        turns += 1;
+    }
 
 }
