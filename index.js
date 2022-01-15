49b0ad2ec0898e77af34e05d5f4a455268a09ceb
Otanga-Otieno <otieno.otanga@gmail.com>
2022-01-15 21:54:11 +0300

    Implement board evaluation function

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function play(cellId) {
         turns += 1;
         winPatternSymbol("X");
     }
+    evaluate();
 
     playRandom();
 
@@ -53,13 +54,15 @@ function AIPlay(cellId) {
     if(winPattern()) return;
     if((turns%2) == 0) {
         drawO(cellId);
-        turns += 1;
         winPatternSymbol("O");
+        turns += 1;
+
     } else {
         drawX(cellId);
-        turns += 1;
         winPatternSymbol("X");
+        turns += 1;
     }
+    evaluate();
 
 }
 
@@ -108,6 +111,48 @@ function winPattern() {
         pattern = true;
         return pattern;
     }
+
+}
+
+function evaluateWinPattern(board) {
+
+    var pattern = false;
+
+    var a1 = board[0];
+    var a2 = board[1];
+    var a3 = board[2];
+    var b1 = board[3];
+    var b2 = board[4];
+    var b3 = board[5];
+    var c1 = board[6];
+    var c2 = board[7];
+    var c3 = board[8];
+
+    if (a1 == b2 && b2 == c3 && b2 != "") {
+        pattern = true;
+    }
+
+    if (a3 == b2 && b2 == c1 && b2 != "") {
+        pattern = true;
+    }
+
+    if (a1 == a2 && a2 == a3 && a1 != ""||b1 == b2 && b2 == b3 && b1 != ""||c1 == c2 && c2 == c3 && c1 != "") {
+        pattern = true;
+    }
+
+    if (a1 == b1 && b1 == c1 && a1 != ""||a2 == b2 && b2 == c2 && a2 != ""||a3 == b3  && b3 == c3 && a3 != "") {
+        pattern = true;
+    }
+
+    if(pattern) {
+        if(turns%2 == 0) {
+            return -10;
+        } else {
+            return 10;
+        }
+    }
+
+    return 0;
 
 }
 
@@ -163,5 +208,18 @@ function playRandom() {
             break;
         }
     }
+
+}
+
+function evaluate() {
+
+    var board = [];
+    for(let i=0; i<9; ++i) {
+        var sym = document.getElementById(stateArrVars[i]).innerHTML;
+        board.push(sym);
+    }
+    var score = evaluateWinPattern(board);
+    console.log(score);console.log(board);
+
 
 }
